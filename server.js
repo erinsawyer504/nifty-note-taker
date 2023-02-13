52ce3cb442576418ea8c6d2e85ee3381214ecc96
erinsawyer504 <erinsawyer504@gmail.com>
2023-02-13 18:40:03 -0500

    added function to create new note and write to file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const { notes } = require('.Develop/db.json');
+const { notes } = require('./Develop/db/db.json');
 
 const PORT = process.env.port || 3001;
 
@@ -10,18 +10,30 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
+function createNewNote (body, notesAdd) {
+    const note = body; 
+    notesAdd.push(note); 
+
+    // path to write file 
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes : notesAdd }, null, 2)
+    );
+    return note; 
+};
+
 // GET Route for homepage
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 );
 
 app.listen(PORT, () =>
