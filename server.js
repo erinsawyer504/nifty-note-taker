4cfab698d6e68053b3302334fbee8588e3173c2b
erinsawyer504 <erinsawyer504@gmail.com>
2023-02-15 18:02:34 -0500

    changed port to PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const notes = require('./db/db.json');
 const uuid = require('./helpers/uuid');
 
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
